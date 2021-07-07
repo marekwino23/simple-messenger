89ae6d7f44e6733ab85cabc89cfc64459b577d0e
marekwino23 <marekw1996@gmail.com>
2021-07-07 16:16:51 +0200

    initial commit and setup socket.io

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import './App.css';
+import React, {useEffect, useState} from "react";
+import io from "socket.io-client"
+let socket;
+const CONNECTION_PORT = 4000;
+
+const App = () => {
+  const [message, setMessage] = useState("")
+  const [username, setUsername] = useState("")
+  const [room, setRoom] = useState("")
+  const [messageList, setMessageList] = useState([])
+  const [loggedIn, setLoggedin] = useState(false)
+
+  const changeMessage = (e) => {
+    setMessage(e.target.value)
+  }
+  const changeUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const changeRoom = (e) => {
+    setRoom(e.target.value)
+    console.log(room)
+  }
+
+  const exitRoom = () => {
+    setLoggedin(false)
+  }
+
+  useEffect(() => {
+    socket = io(`localhost:${CONNECTION_PORT}`);
+    // return () => socket.curentdisconnect();
+  },[]);
+
+  useEffect(() => {
+    socket.on("receive_message", data => {
+      console.log(data)
+    });
+  },[]);
+
+  const connectToRoom = () => {
+    setLoggedin(true)
+    socket.emit("join_room", room, username)
+    if(username !== "" && room !== ""){
+    }
+    else{
+      console.log("empty")
+    }
+   
+  }
+
+  const sendMessage = () => {
+    let messageContent = {
+      room: room,
+      content:{
+        author: username,
+        message: message,
+      },
+    };
+
+    socket.emit("send_message", messageList, messageContent)
+    setMessageList([...messageList, messageContent.content])
+    setMessage("")
+  }
+  
+  // if(!socket) {
+  //   return null;
+  // }
+  return (
+      <div>
+        {!loggedIn ? (
+        <div className="App">
+        <h1 style={{color:"red"}}>You are not logged in</h1>
+        <input required onChange={changeUsername} placeholder="username..." type="text"/>
+        <select required onChange={changeRoom}>
+          <option></option>
+          <option value="Poland">Poland</option>
+          <option value="Germany">Germany</option>
+          <option value="Italy">Italy</option>
+        </select>
+        <button onClick={connectToRoom}>Enter</button>
+      </div>)
+   :(
+     <div>
+     <h1 style={{color:"green"}}>You are logged in {room} <button onClick={exitRoom} className="exit">exit</button></h1>
+    <input onChange={changeMessage} placeholder="message..." type="text"/>
+   <button onClick={sendMessage}>Send</button>
+   <br></br>
+   <br></br>
+     </div>)}
+     </div>
+
+   )}
+
+export default App;
